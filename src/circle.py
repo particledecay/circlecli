12056e861c61e5fa8f4396994cea573b416b6507
Joey Espinosa <jlouis.espinosa@gmail.com>
2016-06-27 00:03:08 -0400

    Adding a basic wrapper for GET requests

---
+++ src/circle.py
@@ -0,0 +1,59 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""CLI wrapper for CircleCI's REST API."""
+import requests
+from urllib import urlencode
+from urlparse import ParseResult, urlparse, urlunparse
+
+
+class CircleAPI(object):
+    """Simple wrapper for requests specific to CircleCI."""
+
+    def __init__(self, token):
+        """Load the API token.
+
+        Args:
+            token (str): the CircleCI API token (obtained at https://circleci.com/account/api)
+        """
+        self._token = token
+        self._base_url = "https://circleci.com/api/v1"
+
+    def build_url(self, endpoint, params={}):
+        """Return the full URL for the desired endpoint.
+
+        Args:
+            endpoint (str): the API endpoint after base URL
+            params (dict): any params to include in the request
+
+        Returns:
+            (str) the full URL of the request
+        """
+        new_params = {'circle-token': self._token}
+        new_params.update(params)
+
+        parsed_url = urlparse(self._base_url)
+        new_parse = ParseResult(scheme=parsed_url.scheme, netloc=parsed_url.netloc,
+                                path='/'.join((parsed_url.path, endpoint)),
+                                params='', query=urlencode(new_params),
+                                fragment='')
+
+        return urlunparse(new_parse)
+
+    def get(self, endpoint, params={}):
+        """Request the URL at `endpoint`.
+
+        Args:
+            endpoint (str): the API endpoint after base URL
+            params (dict): any params to include in the request
+
+        Returns:
+            (dict) the JSON-converted response from the endpoint
+        """
+        url = self.build_url(endpoint, params)
+        headers = {'Content-Type': 'application/json'}
+
+        r = requests.get(url, headers=headers)
+        if r.status_code >= 400:
+            raise Exception(u"There was an error sending this request.")
+
+        return r.json()
