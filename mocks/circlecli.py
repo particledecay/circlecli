a91a9f43b89477d9de24e79ce738edb346dc1bd0
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-11 15:29:31 -0400

    added delete

--- mocks/circlecli.py
+++ mocks/circlecli.py
@@ -11,7 +11,7 @@ from httmock import response, urlmatch
 NETLOC = r'(.*\.)?circleci\.com$'
 HEADERS = {'content-type': 'application/json'}
 GET = 'get'
-
+DELETE = 'delete'
 
 class Resource:
     """ A CircleCli resource.
@@ -34,11 +34,32 @@ class Resource:
         return content
 
 
+    def delete(self):
+        """ Perform a GET request on the resource.
+
+        :rtype: str
+
+        """
+        with open(self.path, 'r') as f:
+            content = f.read()
+        return content
+
 @urlmatch(netloc=NETLOC, method=GET)
 def resource_get(url, request):
     file_path = url.netloc + url.path
     try:
         content = Resource(file_path).get()
+    except EnvironmentError:
+        # catch any environment errors (i.e. file does not exist) and return a
+        # 404.
+        return response(404, {}, HEADERS, None, 5, request)
+    return response(200, content, HEADERS, None, 5, request)
+
+@urlmatch(netloc=NETLOC, method=DELETE)
+def resource_delete(url, request):
+    file_path = url.netloc + url.path
+    try:
+        content = Resource(file_path).delete()
     except EnvironmentError:
         # catch any environment errors (i.e. file does not exist) and return a
         # 404.
