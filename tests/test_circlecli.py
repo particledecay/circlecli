a5bf6bd02a8c3c94d29461a1144eff4c2489f501
Joseph W Becher <drazisil@users.noreply.github.com>
2016-07-10 22:00:58 -0400

    Shell tests for remaining methods

--- tests/test_circlecli.py
+++ tests/test_circlecli.py
@@ -30,6 +30,9 @@ class TestCircleCLI(unittest.TestCase):
     def setUp(self):
         self.circlecli = CircleAPI('bar')
 
+    """ CircleAPI.me()
+        
+    """
     @with_httmock(mocks.circlecli.resource_get)
     def test_me_as_dict(self):
         results = self.circlecli.me(False)
@@ -37,10 +40,414 @@ class TestCircleCLI(unittest.TestCase):
         
         self.assertEqual(results['Username'], 'therealbarack')
 
+    """ CircleAPI.me()
+        
+    """
     @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
     def test_me_as_json(self):
 
         results = self.circlecli.me(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.projects()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_projects_as_dict(self):
+        results = self.circlecli.projects(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.projects()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_projects_as_json(self):
+
+        results = self.circlecli.projects(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.builds()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_builds_as_dict(self):
+        results = self.circlecli.builds(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.builds()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_builds_as_json(self):
+
+        results = self.circlecli.builds(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.artifacts()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_artifacts_as_dict(self):
+        results = self.circlecli.artifacts(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.artifacts()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_artifacts_as_json(self):
+
+        results = self.circlecli.artifacts(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.retry_build()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_retry_build_as_dict(self):
+        results = self.circlecli.retry_build(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.retry_build()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_retry_build_as_json(self):
+
+        results = self.circlecli.retry_build(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.cancel_build()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_cancel_build_as_dict(self):
+        results = self.circlecli.cancel_build(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.cancel_build()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_cancel_build_as_json(self):
+
+        results = self.circlecli.cancel_build(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.ssh_users()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_ssh_users_as_dict(self):
+        results = self.circlecli.ssh_users(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.ssh_users()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_ssh_users_as_json(self):
+
+        results = self.circlecli.ssh_users(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.new_build()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_new_build_as_dict(self):
+        results = self.circlecli.new_build(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.new_build()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_new_build_as_json(self):
+
+        results = self.circlecli.new_build(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.create_ssh()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_create_ssh_as_dict(self):
+        results = self.circlecli.create_ssh(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.create_ssh()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_create_ssh_as_json(self):
+
+        results = self.circlecli.create_ssh(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.list_checkout_keys()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_list_checkout_keys_as_dict(self):
+        results = self.circlecli.list_checkout_keys(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.list_checkout_keys()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_list_checkout_keys_as_json(self):
+
+        results = self.circlecli.list_checkout_keys(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.create_checkout_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_create_checkout_key_as_dict(self):
+        results = self.circlecli.create_checkout_key(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.create_checkout_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_create_checkout_key_as_json(self):
+
+        results = self.circlecli.create_checkout_key(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.checkout_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_checkout_key_as_dict(self):
+        results = self.circlecli.checkout_key(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.checkout_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_checkout_key_as_json(self):
+
+        results = self.circlecli.checkout_key(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.delete_checkout_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_delete_checkout_key_as_dict(self):
+        results = self.circlecli.delete_checkout_key(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.delete_checkout_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_delete_checkout_key_as_json(self):
+
+        results = self.circlecli.delete_checkout_key(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.clear_cache()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_clear_cache_as_dict(self):
+        results = self.circlecli.clear_cache(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.clear_cache()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_clear_cache_as_json(self):
+
+        results = self.circlecli.clear_cache(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.add_circle_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_add_circle_key_as_dict(self):
+        results = self.circlecli.add_circle_key(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.add_circle_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_add_circle_key_as_json(self):
+
+        results = self.circlecli.add_circle_key(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.add_heroku_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_add_heroku_key_as_dict(self):
+        results = self.circlecli.add_heroku_key(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.add_heroku_key()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_add_heroku_key_as_json(self):
+
+        results = self.circlecli.add_heroku_key(True)
+        print results
+
+        data = json.loads(results)
+
+        self.assertEqual(data['login'], 'therealbarack')
+
+    """ CircleAPI.envar()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_envar_as_dict(self):
+        results = self.circlecli.envar(False)
+        print results
+        
+        self.assertEqual(results['Username'], 'therealbarack')
+
+    """ CircleAPI.envar()
+        
+    """
+    @with_httmock(mocks.circlecli.resource_get)
+    @unittest.skip("test not written yet")
+    def test_envar_as_json(self):
+
+        results = self.circlecli.envar(True)
         print results
 
         data = json.loads(results)
