ceb2a5ced3be6bcecb077a5d482b816627b3f85f
Joey Espinosa <jlouis.espinosa@gmail.com>
2016-07-12 23:31:44 -0400

    Updating to v1.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ print repr(install_requires)
 
 setup(
     name='circlecli',
-    version='1.0.1',
+    version='1.1.0',
     url='https://github.com/TheRealJoeLinux/circlecli',
     description=_read('DESCRIPTION'),
     long_description=_read('README.md'),
